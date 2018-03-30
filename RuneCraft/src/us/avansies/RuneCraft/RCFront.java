79188ddd5a4eb2b294884e824c9f4fa5e695a065
Lucas Eastman <lucas546alexander@gmail.com>
2018-03-30 14:25:47 -0700

    Set up plugin directory and config.yml
    Created plugin directory and config File objects in RCFront and set
    their values in onEnable() in order to ensure they exist and are created
    if they don't when the plugin is enabled.

--- RuneCraft/src/us/avansies/RuneCraft/RCFront.java
+++ RuneCraft/src/us/avansies/RuneCraft/RCFront.java
@@ -1,11 +1,29 @@
 package us.avansies.RuneCraft;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RCFront extends JavaPlugin {
 	
+	public static File plugDir;
+	public static File config;
+	
 	public void onEnable() {
-		
+		plugDir = Bukkit.getServer().getPluginManager().getPlugin("RuneCraft").getDataFolder();
+		if(!plugDir.exists()) {
+			plugDir.mkdir();
+		}
+		config = new File(plugDir.getPath() + File.separator + "config.yml");
+		if(!config.exists()) {
+			try {
+				config.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 	
 }
