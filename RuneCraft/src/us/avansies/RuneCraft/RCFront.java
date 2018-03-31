444699f43ac330a4a9f9f0f4f589a15869c93411
Lucas Eastman <lucas546alexander@gmail.com>
2018-03-30 19:02:43 -0700

    Wrote comments explaining code
    Wrote comments explaining what is being done with specific parts of code
    in RCFront.java

--- RuneCraft/src/us/avansies/RuneCraft/RCFront.java
+++ RuneCraft/src/us/avansies/RuneCraft/RCFront.java
@@ -11,14 +11,21 @@ public class RCFront extends JavaPlugin {
 	public static File plugDir;
 	public static File config;
 	
+//	For enabling the plugin
 	public void onEnable() {
+//		Set the value of plugDir to the data folder corresponding to the plugin
 		plugDir = Bukkit.getServer().getPluginManager().getPlugin("RuneCraft").getDataFolder();
+//		Check to see if the folder exists
 		if(!plugDir.exists()) {
+//			Create a directory out of the plugin directory
 			plugDir.mkdir();
 		}
+//		Set the value of config to a file within the plugin directory by the name of "config.yml"
 		config = new File(plugDir.getPath() + File.separator + "config.yml");
+//		Check to see if config.yml exists
 		if(!config.exists()) {
 			try {
+//				Create a file out of config
 				config.createNewFile();
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
